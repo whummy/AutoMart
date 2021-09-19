8cc2ed4b60d3110567261545db3d2b62d44940a7
DELL <DELL@DESKTOP-I6F02J5>
2021-09-19 21:44:05 +0100

    Add project files.

---
+++ Entities/Models/Cars/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models.Cars
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+    }
+}
