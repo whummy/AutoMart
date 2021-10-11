c92837c1fa890a01b958cf37d9e0f6b83488fbe9
whummy <aparaolawunmi@gmail.com>
2021-10-11 12:08:09 +0100

    chnages

---
+++ Entities/Models/ClaimTypeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ClaimTypeHelper
+    {
+        public static string UserId { get; set; } = "UserId";
+        public static string Email { get; set; } = "Email";
+        public static string FirstName { get; set; } = "FirstName";
+        public static string LastName { get; set; } = "LastName";
+        public static string Roles { get; set; } = ClaimTypes.Role;
+    }
+}
